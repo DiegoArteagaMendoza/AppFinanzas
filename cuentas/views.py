dca0f1363dd493030e7472654bc34bfbc29f226e
diegoarteagamendoza <daarteagamendoza@gmail.com>
2026-01-03 19:45:20 -0300

    actualización de authenticación en cuentas

--- cuentas/views.py
+++ cuentas/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from .queryset import CuentasQueryset
-
+from varios.authentication import CustomJWTAuthentication
 """
 Registrar Cuenta
 """
 
 @api_view(['POST'])
+@authentication_classes([CustomJWTAuthentication])
+@permission_classes([IsAuthenticated])
 def crear_cuenta(request):
     return CuentasQueryset.registrar_cuenta(request)
 
@@ -20,6 +23,8 @@ Buscar Cuentas
 """
 
 @api_view(['GET'])
+@authentication_classes([CustomJWTAuthentication])
+@permission_classes([IsAuthenticated])
 def buscar_cuenta(request):
     return CuentasQueryset.buscar_cuentas(request)
 
@@ -27,5 +32,7 @@ def buscar_cuenta(request):
 Actualizar Cuenta
 """
 @api_view(['PUT'])
+@authentication_classes([CustomJWTAuthentication])
+@permission_classes([IsAuthenticated])
 def actualizar_cuenta(request):
     return CuentasQueryset.actualizar_cuenta(request)
