52a7f055bd0b321b03792172b66e0a0faaf0d8fe
diegoarteagamendoza <daarteagamendoza@gmail.com>
2026-01-03 19:43:34 -0300

    actualización de JWT en usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from .queryset import UsuarioQueryset, IniciarSesion
+from varios.authentication import CustomJWTAuthentication
 
 """
 Login
 """
 
 @api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
 def login(request):
     return IniciarSesion.login(request)
 
@@ -20,9 +24,13 @@ Usuario
 """
 
 @api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
 def registrar_usuario(request):
     return UsuarioQueryset.registrar_usuario(request)
 
 @api_view(['PUT'])
+@authentication_classes([CustomJWTAuthentication])
+@permission_classes([IsAuthenticated])
 def actualizar_usuario(request):
     return UsuarioQueryset.actualizar_usuario(request)
